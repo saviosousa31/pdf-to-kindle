74d2c4e3463b581a34563f3b0583228a49464440
Sávio Soares Sousa <ssoaressousas@gmail.com>
2026-04-13 19:47:44 -0300

    Revert "Update App.kt"
    This reverts commit 274dd5c0c600bd04de61705e4a1dd8442ccedd23.

--- android-project/app/src/main/java/com/pdfepub/converter/App.kt
+++ android-project/app/src/main/java/com/pdfepub/converter/App.kt
@@ -7,16 +7,8 @@ import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        try {
-            PDFBoxResourceLoader.init(applicationContext)
-        } catch (e: Exception) {
-            android.util.Log.e("App", "Erro ao inicializar PDFBox: ${e.message}")
-        }
-        try {
-            Prefs.applyDarkMode(this)
-        } catch (e: Exception) {
-            android.util.Log.e("App", "Erro ao aplicar dark mode: ${e.message}")
-        }
+        PDFBoxResourceLoader.init(applicationContext)
+        Prefs.applyDarkMode(this)
     }
 
     override fun attachBaseContext(base: Context) {
